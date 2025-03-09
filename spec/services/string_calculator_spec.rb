4d01f7798263465a2c3c135376e4a2cfd055fc43
Aishwarya <aishpriya2516@gmail.com>
2025-03-09 20:08:13 +0530

    This test asserts that the add method can handle an unknown number of numbers seperated by commas 1,2,3,4,5, it should return 15(the sum of 1,2,3,4 and 5).

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 require "string_calculator"
 
 RSpec.describe StringCalculator do
+	
   describe ".add" do
+
 	    it "returns 0 for an empty string" do
 	       expect(StringCalculator.add("")).to eq(0)
 	    end
@@ -14,5 +16,9 @@ RSpec.describe StringCalculator do
         it "returns the sum of two numbers" do
           expect(StringCalculator.add("1,5")).to eq(6)
         end
+
+	    it "handles an unknown number of numbers" do
+	      expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+	    end        
 	end
 end
