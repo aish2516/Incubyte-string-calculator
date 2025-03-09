f73e378b20f1f46dc39ae61251efd30b69b5600b
Aishwarya <aishpriya2516@gmail.com>
2025-03-09 20:17:08 +0530

    This test asserts that the add method can handle custom  delimiters specified at the beginning of the string(e.g., //

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -19,10 +19,15 @@ RSpec.describe StringCalculator do
 
 	    it "handles an unknown number of numbers" do
 	      expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
-	    end 
+	    end
 
 	    it "allows new lines as delimiters" do
 	      expect(StringCalculator.add("1\n2,3")).to eq(6)
-	    end	           
+	    end
+
+	    it "supports different delimiters" do
+	      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+	    end
+           
 	end
 end
