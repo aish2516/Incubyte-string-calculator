28efaa8653a9b35b005309b780e43f2a7f470222
Aishwarya <aishpriya2516@gmail.com>
2025-03-09 20:24:46 +0530

    This test asserts that the add method raises an Argument Error with the message 'Negative numbers not allowed', when negative numbers are passed as input.

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -28,6 +28,10 @@ RSpec.describe StringCalculator do
 	    it "supports different delimiters" do
 	      expect(StringCalculator.add("//;\n1;2")).to eq(3)
 	    end
-           
+
+	    it "raises an error for negative numbers" do
+	      expect { StringCalculator.add("-1,2,-3") }.to raise_error(ArgumentError, "Negative numbers not allowed: -1, -3")
+	    end	    
+
 	end
 end
